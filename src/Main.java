3e97e8c995ae4444f9d6d2907e0cd186a8949b63
vic-cavalcant3 <victorrocha0223@gmail.com>
2025-05-24 17:10:08 -0300

    feat: primeiro codigo java

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "victor";
+        System.out.println(nome);
+
+        int valor01 = 1;
+        int valor02 = 4;
+
+        double valor03 = 10;
+        double valor04 = 18;
+
+        System.out.println(valor01 + valor02 + valor03);
+        System.out.println(valor01 - valor02 - valor03);
+        System.out.println(valor01 / valor02);
+        System.out.println(valor01 * valor02 * valor03);
+
+
+        String letra = "abcdefg";
+        int sup = 999999999;
+        long sup01 = 999999L;
+
+        float sup03 = 9.9F;
+        double sup04 = 9999999999999999999.9;
+
+
+
+    }
+}
